199b8f38f7aeabbb651475f0392d9af677376833
Андрей Кустов <kustov.p4ssport@yandex.ru>
2022-02-09 16:53:51 +0300

    поменял ссылку для апи cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://0djfkkqql7.execute-api.eu-west-1.amazonaws.com/dev/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://cart-service-app-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
